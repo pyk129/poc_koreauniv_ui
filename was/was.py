4533b3b7fd526afccdcc399cc89e94aeeb26a9d2
yk <embededmaster@gmail.com>
2020-07-16 11:43:40 +0900

    gogo

--- was/was.py
+++ was/was.py
@@ -848,21 +848,21 @@ class Recommend1:
                     yyyy = int(strYYYY)
                     
                     room_size = str(d['전용면적(㎡)'])
-                    room_size_comma_idx = room_size.index('.')
-                     
-                    
+# =============================================================================
+#                     room_size_comma_idx = room_size.index('.')
+# =============================================================================
                     if int(d['거래금액(만원)']) >= int(maxPrice):
-                         maxRoomSize = room_size[:room_size_comma_idx]
+                         maxRoomSize = room_size
                          
                     if int(d['거래금액(만원)']) <= int(minPrice):
-                         minRoomSize = room_size[:room_size_comma_idx]
+                         minRoomSize = room_size
                          
                     if yyyy < 2020:                            
                          jisuItem = {}
                          jisuItem['date'] = str(int(d['계약년월']))
                          jisuItem['jisu'] = str(d['매매가격지수'])
                          jisuItem['realprice'] = str(int(d['거래금액(만원)']))
-                         jisuItem['room_size'] = room_size[:room_size_comma_idx]
+                         jisuItem['room_size'] = room_size
 
     # =============================================================================
     #                      jisuItem['predict_jisu'] = str(predict[index])
@@ -874,11 +874,13 @@ class Recommend1:
                          jisuItem = {}
                          jisuItem['date'] = str(int(d['계약년월']))
                          jisuItem['realprice'] = str(int(d['거래금액(만원)']))     
-                         jisuItem['room_size'] = room_size[:room_size_comma_idx]                                  
+                         jisuItem['room_size'] = room_size                                 
                          maemae_jisu_2020.append(jisuItem)
 
                         
-                    print('[',room_size[:room_size_comma_idx],']  ', jisuItem['realprice'])
+# =============================================================================
+#                     print('[',room_size[:room_size_comma_idx],']  ', jisuItem['realprice'])
+# =============================================================================
 # =============================================================================
 #                 recentRoomSize = str(dfAptInfo.iloc[i]['전용면적(㎡)'])
 # =============================================================================
@@ -1607,8 +1609,8 @@ multi_nbc = Pipeline([('vect', CountVectorizer(ngram_range=(1, 2),stop_words=sto
 # 
 # multi_nbc = Pipeline([('vect', CountVectorizer(ngram_range=(1, 2),stop_words=stopword, tokenizer=tokenizer_mecab_morphs)),
 #                       ('nbc', MultinomialNB())])
-# 
 # =============================================================================
+
 
 
 if __name__ == "__main__":
